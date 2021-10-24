dd10b0df4859e2e7b6a3bb447a2c644df25564f8
samconnects1 <samconnects@gmail.com>
2021-10-25 11:06:39 +1300

    user api working

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ const express = require('express')
 require('./db/mongoose')
 const User = require('./models/user')
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3001
 app.use(express.json())
+
+
 
 app.post('/users',(req,res)=>{
     // res.send('testing!')
