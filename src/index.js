88a185ef4c10927ca32941d957c5020a3ace7419
samconnects1 <samconnects@gmail.com>
2021-10-25 18:06:14 +1300

    added patch

--- src/index.js
+++ src/index.js
@@ -31,13 +31,17 @@ app.get('/users',async (req,res)=>{
    }
 
 })
+
+
+
+
 app.get('/users/:id',async (req,res)=>{
     // console.log(req.params)
     const _id = req.params.id
     try{
         const user = await User.findById(_id)
         if(!user){
-            return res.status(404).send()
+            return res.status(404).send('No such user')
         }
         res.send(user)
 
@@ -47,6 +51,29 @@ app.get('/users/:id',async (req,res)=>{
 
     }
 )
+
+app.patch('/users/:id', async (req,res)=>{
+    console.log(req.body)
+    const allowedUpdates =['name','email','age','password']
+    const updates = Object.keys(req.body)
+    const isValidOperation = updates.every((update)=>{
+      return allowedUpdates.includes(update)
+    })
+    if(!isValidOperation){
+        return res.status(400).json({item:"no such property"})
+    }
+    try{
+        const user = await User.findByIdAndUpdate(req.params.id,req.body,{new:true,runValidators:true})
+        if(!user){
+            return res.status(404).json({name:'User Doesnt exist!'})
+        }res.send(user)
+    }
+    catch(e){
+        res.status(400).send(e)
+    }
+})
+
+
 app.get('/tasks/:id', async (req,res)=>{
     // console.log(req.params)
     const _taskId = req.params.id
